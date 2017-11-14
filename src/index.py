afbaee87ff8cc4862bc24344198ff2639269a698
Jacob Boomgaarden <jcboomgaarden@gmail.com>
2017-11-14 16:34:06 +0200

    added users & product_lines tables to database

--- src/index.py
+++ src/index.py
@@ -16,6 +16,31 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 with app.app_context():
     db.init_app(app)
 
+class User(db.Model):
+    __tablename__ = 'users'
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    social_id = db.Column(db.String(64), nullable=False, unique=True)
+    nickname = db.Column(db.String(64), nullable=False)
+    email = db.Column(db.String(64), nullable=False)
+
+    def __repr__(self):
+        return str({'id':self.id, 'social_id':self.social_id, 'nickname':self.nickname, 'email':self.email})
+
+class ProductLines(db.Model):
+    __tablename__ = 'product_lines'
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    name = db.Column(db.String(64), nullable=False, unique=True)
+    description = db.Column(db.String(64), nullable=True, unique=False)
+
+    def __repr__(self):
+        return str({'id':self.id, 'name':self.name, 'description':self.description})
+
+
+with app.app_context():
+    db.create_all()
+    db.session.commit()
+
+
 @app.route("/login")
 def login():
     return render_template("login.html")
