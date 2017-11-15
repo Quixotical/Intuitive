8e4068110d181957157d8299f6d5b9fce2f65310
Jacob Boomgaarden <jcboomgaarden@gmail.com>
2017-11-15 19:25:32 +0200

    moved config values to separate file

---
+++ src/config.py
@@ -0,0 +1,9 @@
+OAUTH_CREDENTIALS = {
+    'facebook': {
+        'id': '1409261805838052',
+        'secret': '909e62e688ed787024782ea152a9cb87'
+    }
+}
+
+SQLALCHEMY_DATABASE_URI = 'mysql://root:supersecure@db/feature_db'
+SQLALCHEMY_TRACK_MODIFICATIONS = False
